f18ff2cb8d7c0799532b20573462111b34593db1
Gaveen Nanayakkara <gaveennanayakkara@Gaveens-MacBook-Pro-M1.local>
2025-01-09 19:25:23 +0530

    Firebase Set up

---
+++ configs/FirebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCw3RL6xA9vI2wQJHSjDMWsOM22nVTUCqE",
+  authDomain: "event-payment-35c02.firebaseapp.com",
+  projectId: "event-payment-35c02",
+  storageBucket: "event-payment-35c02.firebasestorage.app",
+  messagingSenderId: "1086113085981",
+  appId: "1:1086113085981:web:ccc59dd17102a3c81b24f4"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
