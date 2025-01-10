2de2acafab7e782508d850b1a9768cbcee7094e2
Gaveen Nanayakkara <gaveennanayakkara@Gaveens-MacBook-Pro-M1.local>
2025-01-10 19:02:24 +0530

    Categories

--- app/(tabs)/Home.jsx
+++ app/(tabs)/Home.jsx
@@ -2,17 +2,17 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import Header from '../../components/Home/Header'
 import Slider from '../../components/Home/Slider'
+import Category from '../../components/Home/Category'
 
 export default function Home() {
   return (
     <View>
       {/* {Header} */}
-      <Header />
-
+        <Header />
       {/* {Slider} */}
-      <Slider />
+        <Slider />
       {/* {Category} */}
-
+      <Category />
       {/* {Explore} */}
 
     </View>
